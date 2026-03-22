5b09da809db68f064a2520187d4523754a507a0b
diyushuang <46947746+diyushuang@users.noreply.github.com>
2026-03-22 21:29:42 +0800

    使用Union[str, None]类型注释修复LLM函数工具参数缺少类型注释的问题

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Union
 from astrbot.api.event import filter
 from astrbot.api.star import Context, Star
 from astrbot.api import logger, AstrBotConfig
@@ -409,7 +410,7 @@ class CloudflareImgbedRandomPlugin(Star):
             yield event.plain_result(f"命令处理失败: {str(e)}")
     
     @filter.llm_tool(name="sendRandomMedia")
-    async def send_random_media(self, event: object, directory: str, content_type: str):
+    async def send_random_media(self, event, directory: Union[str, None] = None, content_type: Union[str, None] = None):
         '''发送随机图片或视频
 
         当用户请求随机图片或视频时使用此工具，例如：
